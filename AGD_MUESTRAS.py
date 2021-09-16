59758d58a533bfc4c708c0ee882bf2bfcd7e00ad
mayra9821 <76023578+mayra9821@users.noreply.github.com>
2021-09-16 08:01:04 -0500

    AGD_MEUSTRAS funcionando

--- AGD_MUESTRAS.py
+++ AGD_MUESTRAS.py
@@ -17,8 +17,6 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base
 
 with monitoreo.connect() as connection2:
-    
-    
     query2 = "SELECT ID_MUESTREO, ID_CUALIDAD, MUESTREOSOBSERVACIONES, REPLICA FROM VM_DATOS_MONITOREO WHERE ID_PROYECTO = 2148 "
     query2Result = connection2.execute(query2)
     datos2 = query2Result.fetchall()
@@ -26,26 +24,24 @@ with monitoreo.connect() as connection2:
     datos2Df.columns = [colName.upper() for colName in query2Result.keys()]                                                                                                                                          
     datos2Df['COMPLEMENTO'] = datos2Df['ID_CUALIDAD'].str.extract(r'((?=\s).*)', expand = False).apply(lambda x: x.strip().replace(",","")[2:])
     datos2Df['ID_MUESTRA'] = datos2Df['ID_MUESTREO'].astype('str') + datos2Df['COMPLEMENTO'].astype('str')
+    muestras = list()
     # print(datos2Df['ID_MUESTRA'])
     # agd_muestras = pd.DataFrame(columns = ['ID_MUESTRA','ID_MUESTREO','NOTAS','ES_REPLICA'])
-    muestras = list()
     # print(datos2Df['ID_MUESTRA'].unique().size)
     for _, df_muestra in datos2Df.groupby('ID_MUESTRA'):
         
         insertMuestra = f"""INSERT INTO AGD_MUESTRAS (ID_MUESTRA, ID_MUESTREO, NOTAS, ES_REPLICA)
-                        VALUES({df_muestra['ID_MUESTRA'].values[0]},{df_muestra['ID_MUESTREO'].values[0]},{df_muestra['MUESTREOSOBSERVACIONES'].values[0]},{df_muestra['REPLICA'].values[0]})"""
+                        VALUES({df_muestra['ID_MUESTRA'].values[0]},{df_muestra['ID_MUESTREO'].values[0]},{'null'},{df_muestra['REPLICA'].values[0]})"""
         
         muestras.append(insertMuestra)
     muestras = pd.DataFrame(data=muestras, columns = ['SQL'])                  
-    print(muestras)
+    # print(muestras)
     # print(pd.DataFrame(datos2Df['ID_MUESTRA']))
     # pd.DataFrame(datos2Df['ID_MUESTRA']).to_csv('muestras.csv', index=False)
     muestras.to_csv('muestras.csv', index=False)
 
 # with engine.connect() as connection:
 
-    # for index, row in insertMuestra.iterrows():
-    #     connection.execute(row['SQL'])
+#     for index, row in insertMuestra.iterrows():
+#         connection.execute(row['SQL'])
 #         print(row[])
-
-    
